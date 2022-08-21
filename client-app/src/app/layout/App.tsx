be045a83a14e35841b4d1f3c7a4bd51bf183eb7d
Abolfazl Karimi <ab.karimi@cbi.ir>
2022-08-21 17:57:44 +0430

    axios config

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,23 +1,36 @@
 import React, { Fragment, useEffect, useState } from 'react';
-
-import axios from 'axios';
 import { Container} from 'semantic-ui-react'
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([])
   const [selectedActivity, setSelectedActivity]= useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(()=>{
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response =>{
-      setActivities(response.data);
-      console.log(response.data);
-    })
+    // axios.get<Activity[]>('http://localhost:5000/api/activities').then(response =>{
+    //   setActivities(response.data);
+    //   console.log(response.data);
+    // })
+      agent.Activities.list().then(response => {
+
+        let activities:Activity[] = [];
+
+        response.forEach((activity) => {
+          activity.date = activity.date.split('T')[0];
+          activities.push(activity);
+          
+        });
+        setActivities(activities);
+        setLoading(false);
+      });
   },[]);
 
   const handleSelectActivity = (id:string)=>{
@@ -47,6 +60,7 @@ function App() {
     setActivities([...activities.filter(q=>q.id !== id)]);
 
   }
+  if(loading) return <LoadingComponent content='Loading App'/>
   return (
     <Fragment>
       <NavBar openForm = {handleFormOpen}/>
